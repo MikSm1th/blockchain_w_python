d99f0abadc48aa217adc013872b4dd4f8b120793
MikSm1th <thiscuriousquest@gmail.com>
2021-01-26 20:48:22 -0500

    Added keys

---
+++ ch1/verify_message.py
@@ -0,0 +1,9 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization 
+
+GENERATE_PRIVATE_KEY = False
+DERIVE_PUBLIC_KEY_FROM_PRIVATE_KEY = False
+P
